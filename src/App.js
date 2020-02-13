c40159faf8a322a488718a25c58cb754f7628d1c
James <james.duong93@gmail.com>
2020-02-13 20:18:54 +1100

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
+// first, we will make a new context
+const MyContext = React.createContext();
+
+// then, create a provider component
+class MyProvider extends Component {
+  state = {
+    name: "Wes",
+    age: 100,
+    cool: true
+  };
+  render() {
+    return (
+      <MyContext.Provider
+        value={{
+          state: this.state,
+          growAYearOlder: () => {
+            this.setState({ age: this.state.age + 1 });
+          }
+        }}
+      >
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+}
+
+const Family = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="family">
+      <Person />
     </div>
   );
+};
+
+class Person extends Component {
+  render() {
+    return (
+      <div className="person">
+        <MyContext.Consumer>
+          {context => (
+            <React.Fragment>
+              <p>Name: {context.state.name}</p>
+              <p>Age: {context.state.age}</p>
+              <button onClick={context.growAYearOlder}>Increase</button>
+            </React.Fragment>
+          )}
+        </MyContext.Consumer>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <MyProvider>
+        <div>
+          <p>i'm the app</p>
+          <Family />
+        </div>
+      </MyProvider>
+    );
+  }
 }
 
 export default App;
